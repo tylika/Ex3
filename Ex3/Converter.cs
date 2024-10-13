785aff3d5628885ac58d91ae6f1fba76f5de955a
tylika <yuliana03velikaya12@gmail.com>
2024-10-14 01:58:55 +0300

    Add project files.

---
+++ Ex3/Converter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Converter
+{
+    private decimal euro;
+    private decimal dolar;
+
+    public decimal Euro { get
+        {
+            return euro;
+        }
+        set
+        {
+            if (value < 0) euro = 0;
+            else euro = value;
+        }
+    }
+    public decimal Dolar
+    {
+        get
+        {
+            return dolar;
+        }
+        set
+        {
+            if (value < 0) dolar = 0;
+            else dolar = value;
+        }
+    }
+    Dictionary<string, decimal> ExchangeRate;
+    public Converter(decimal euro1, decimal dolar1)
+    {
+        ExchangeRate = new Dictionary<string, decimal>
+        {
+            {"EUR", euro1 },
+            {"USD", dolar1 }
+        };
+    }
+
+    public decimal ConvertToUah(decimal amount, string currency)
+    { decimal rate = 0;
+        if (!ExchangeRate.TryGetValue(currency, out rate)) { Console.WriteLine("Невiдома валюта, повернiться на початок, щоб додати"); }
+        decimal result = rate*amount;
+        return result;
+    }
+    public decimal ConvertFromUah(decimal amount, string currency)
+    {
+        decimal rate = 0;
+        if (!ExchangeRate.TryGetValue(currency, out rate)) { Console.WriteLine("Невiдома валюта, повернiться на початок, щоб додати"); return 0; }
+        decimal result = amount / rate;
+        return result;
+    }
+    public void AddOrEditCurrency(decimal amount, string currency)
+    {
+        ExchangeRate[currency]  = amount;
+    }
+    public void ShowCurrency()
+    {
+        string res = "";
+        foreach(KeyValuePair<string, decimal> item in ExchangeRate)
+        {
+            res = "1 " + item.Key +" = " + item.Value + " грн";
+            Console.WriteLine(res);
+        }
+       
+    }
+}
+
---
+++ Ex3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;    
+        Converter converter = new Converter(45.0m, 41.0m);
+        while (true)
+        {
+            Console.WriteLine("Оберiть дію");
+            Console.WriteLine("1.Конвертувати валюту в гривнi");
+            Console.WriteLine("2.Конвертувати гривні в валюту ");
+            Console.WriteLine("3.Додати нову валюту, або змiнити курс");
+            Console.WriteLine("4.Подивитися доступні валюти");
+            Console.WriteLine("5.Завершити роботу");
+            int choise = int.Parse(Console.ReadLine());
+            if (choise == 1)
+            {
+                ConvertToUah(converter);
+            }
+            else if (choise == 2)
+            {
+                ConvertFromUah(converter);
+            }
+            else if (choise == 3)
+            {
+                AddOrEditCurrency(converter);
+            }
+            else if (choise == 4)
+            {
+                converter.ShowCurrency();
+            }
+            else if (choise == 5) 
+            {
+                return; 
+            }
+            else
+            {
+                Console.WriteLine("Введіть потрiбний номер зi спискy");
+            }
+        }
+        void ConvertToUah(Converter converter)
+        {
+            decimal amount;
+            string currency;
+            Console.WriteLine("Введiть валюту");
+            currency = Console.ReadLine();
+            Console.WriteLine($"Введіть суму в {currency}");
+            amount = decimal.Parse(Console.ReadLine());
+            decimal result = converter.ConvertToUah(amount, currency);
+            if (result>0) Console.WriteLine($"{amount} {currency} = {result} грн");
+        }
+        void ConvertFromUah(Converter converter)
+        {
+            decimal amount;
+            string currency;
+            Console.WriteLine("Введiть валюту");
+            currency = Console.ReadLine();
+            Console.WriteLine("Введіть суму в грн");
+            while (!decimal.TryParse(Console.ReadLine(), out amount) || amount<=0)
+            {
+                Console.WriteLine("Введіть коректну суму.");
+            }
+            decimal result = converter.ConvertFromUah(amount, currency);
+            if (result > 0)  Console.WriteLine($"{amount} грн = {result} {currency}");
+        }
+        void AddOrEditCurrency (Converter converter)
+        {
+            string currency;
+            decimal rate= 0;
+            Console.WriteLine("Введiть валюту");
+            currency= Console.ReadLine();
+            Console.WriteLine($"Введiть курс {currency}");
+            rate = decimal.Parse(Console.ReadLine());
+            converter.AddOrEditCurrency(rate, currency);
+            Console.WriteLine("done");
+        }
+
+    }
+}
